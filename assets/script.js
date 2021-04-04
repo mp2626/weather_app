7af78f2de0f5785e097716f605afdfc9d6bbeb6d
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-04-04 17:47:39 +1000

    Update create cards function to filter out unwanted data

--- assets/script.js
+++ assets/script.js
@@ -8,27 +8,46 @@ const apiKey = "&units=metric&appid=dafe53ce7645ef5b27a79562190e601b";
 // store search
 function getForecast(event) {
     event.preventDefault()
-    let city = $(event.target).parents().find("input").val().toLowerCase()
+    let city = $(event.target).parents().find("input").val().toLowerCase().trim()
     searchApi = api + city + apiKey
 
     fetch(searchApi)
         .then(function (response) {
             if (response.ok) {
-                createCards(response.json())
+                response.json().then(function (data) {
+                    createCards(data);
+                })
             } else {
                 alert("No result found for " + city)
-            }
+            };
         })
 }
 
 // create cards and set attributes
 function createCards(data) {
-    console.log(data)
+    // console.log(data);
+    let currentDate = '';
+    let count = 0;
+    console.log(currentDate);
+    console.log(data.list);
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.list.length; i++) {
+        // gather data in vars
+        // date = data;
+        if (currentDate !== moment(data.list[i].dt_txt).format('DD-MM-YYYY') && count <= 4) {
+            date = moment(data.list[i].dt_txt).format('DD-MM-YYYY')
+            console.log(date);
+            temp = data.list[i].main.temp;
+            console.log(temp)
 
 
 
+            currentDate = moment(data.list[i].dt_txt).format('DD-MM-YYYY')
+            count++
+        }
+        // build cards
+
+        // assign data
     }
 
 
